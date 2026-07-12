c35ec7e0d798dec9ff82ecec5f83eabe0eeca6d2
Catalin Flintasu <catalinfl02@gmail.com>
2026-07-13 02:26:08 +0300

    index.ts for wayfinder

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { buildTrie, splitPath, type Route, type TrieNode } from "./trie";
+export { match, matchPath, type MatchResult } from "./match";
+export { createCachedMatcher, type CachedMatcher } from "./cache";
+export { createRouterStore, type RouterStore, type RouterState } from "./store";
+export {
+    lazy,
+    isLazy,
+    useRouter,
+    ROUTER_CONTEXT,
+    type AppRoute,
+    type RouteHandler,
+    type LazyWrapper,
+    type Component,
+} from "./router";
+export { default as Router } from "./Router.svelte"
+export { default as Link } from "./Link.svelte";
